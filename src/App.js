7b6d6b9ef08a289e78d008b6a4a7204541c581e3
unknown <101saurav.kumar@gmail.com>
2023-08-12 13:21:15 +0530

    recover

--- src/App.js
+++ src/App.js
@@ -1,72 +1,29 @@
-// import { BrowserRouter as Router, Routes , Route } from 'react-router-dom';
-// import Home from './pages/home';
-// import Project from './pages/project';
-// import Experiences from './pages/experiences';
-// import Navbar from './components/navbar';
-// import Footer from './components/Footer';
-// import InstagramPage from './pages/instagram';
-// import ProjectDisplay from './pages/ProjectDisplay';
+import { BrowserRouter as Router, Routes , Route } from 'react-router-dom';
+import Home from './pages/home';
+import Project from './pages/project';
+import Experiences from './pages/experiences';
+import Navbar from './components/navbar';
+import Footer from './components/Footer';
+import InstagramPage from './pages/instagram';
+import ProjectDisplay from './pages/ProjectDisplay';
 import './App.css';
 
-import { useEffect } from 'react';
-import axios from 'axios';
-import querystring from 'querystring';
-
 function App() {
-
-  const clientId = '279332924797649';
-  const redirectUri = 'https://portfolio-six-zeta-86.vercel.app/';
-  const authEndpoint = 'https://api.instagram.com/oauth/authorize/';
-  // const scope = 'user_profile,user_media';
-  const scope= "user_profile,user_media"
-  const handleAuthorization = () => {
-    const queryParams = querystring.stringify({
-      client_id: clientId,
-      redirect_uri: redirectUri,
-      response_type: 'code',
-      scope: scope,
-    });
-
-    const authorizationUrl = `${authEndpoint}?${queryParams}`;
-   
-    window.location.href = authorizationUrl;
-  };
-
-  useEffect(() => {
-    // Check if the page was redirected with the authorization code
-    
-    const authorizationCode = new URLSearchParams(window.location.search).get('code');
-    console.log("authorizationCode" , authorizationCode);
-    if (authorizationCode) {
-      // Use the authorization code to get an access token
-      const tokenEndpoint = 'https://api.instagram.com/oauth/access_token';
-
-      const tokenParams = {
-        client_id: clientId,
-        client_secret: '8a3a475b865e9661da708abaf1c147af',
-        grant_type: 'authorization_code',
-        redirect_uri: redirectUri,
-        code: authorizationCode,
-      };
-
-      axios.post(tokenEndpoint, querystring.stringify(tokenParams))
-        .then(response => {
-          const accessToken = response.data.access_token;
-          // Now you can use the accessToken to make API calls to Instagram
-          console.log('Access Token:', accessToken);
-        })
-        .catch(error => {
-          console.error('Error fetching access token:', error);
-          console.log("authorizationCode" , authorizationCode);
-        });
-    }
-  }, []);
-
-
   return (
-    <div>
-      <h1>Instagram AUthorization</h1>
-      <button onClick={handleAuthorization}>Authorize with Instagram</button>
+    <div className="App">
+     
+      <Router>
+      <Navbar/>
+      <Routes>
+      <Route path="/"  element={<Home/>} />
+      <Route path="/projects"  element={<Project/>} />
+      <Route path="/projects/:id"  element={<ProjectDisplay/>} />
+      <Route path="/experience"  element={<Experiences/>} />
+      {/* <Route path="/instagram"  element={<InstagramPage/>} /> */}/
+      </Routes>
+      <Footer/>
+      </Router>
+      
     </div>
   );
 }
